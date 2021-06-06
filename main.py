5375793b852efd1224eb1823d6bf6dac19f83073
IvanZuev <ivan78.ru@yandex.ru>
2021-06-06 17:31:40 +0300

    Интеграция БД в систему. Уже подтягивает типы и вещества из БД.

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ import numpy as np
 import pandas as pd
 
 
-Smola_name = ['KER-828', 'LaprBD', 'YDPN-638']
-Smola = np.array([0.8, 0.2, 0])
+Epoxy_name = ['KER-828', 'LaprBD', 'YDPN-638']
+Epoxy = np.array([0.8, 0.2, 0])
 Amine_name = ['IPDA', 'MXDA', 'PACM']
 Amine = np.array([0.5, 0.5, 0])
 
 # Amin = np.array([1,2,3])
-# c = np.outer(Smola, Amine)
+# c = np.outer(Epoxy, Amine)
 #
-# print(Smola, Amine, c, sep='\n')
+# print(Epoxy, Amine, c, sep='\n')
 
 
-df = pd.DataFrame(np.outer(Smola, Amine), index=Smola_name, columns=Amine_name, )
+df = pd.DataFrame(np.outer(Epoxy, Amine), index=Epoxy_name, columns=Amine_name, )
 # df.columns = Amine_name
-# df.rename({ i:Smola_name[i] for i in range(len(Smola_name))})
+# df.rename({ i:Epoxy_name[i] for i in range(len(Epoxy_name))})
 print(df)
 print('-------------')
-# print(Amine_name[1], '+', Smola_name[2], df[Amine_name[1]][Smola_name[2]])
+# print(Amine_name[1], '+', Epoxy_name[2], df[Amine_name[1]][Epoxy_name[2]])
 
 
-df_tg_base = pd.DataFrame(index=Smola_name, columns=Amine_name)
+df_tg_base = pd.DataFrame(index=Epoxy_name, columns=Amine_name)
 
-df_tg_base[Amine_name[0]][Smola_name[0]] = 148
-df_tg_base[Amine_name[0]][Smola_name[1]] = -10
-df_tg_base[Amine_name[0]][Smola_name[2]] = 165
-df_tg_base[Amine_name[1]][Smola_name[0]] = 123
-df_tg_base[Amine_name[1]][Smola_name[1]] = -10
-df_tg_base[Amine_name[1]][Smola_name[2]] = 185
+df_tg_base[Amine_name[0]][Epoxy_name[0]] = 148
+df_tg_base[Amine_name[0]][Epoxy_name[1]] = -10
+df_tg_base[Amine_name[0]][Epoxy_name[2]] = 165
+df_tg_base[Amine_name[1]][Epoxy_name[0]] = 123
+df_tg_base[Amine_name[1]][Epoxy_name[1]] = -10
+df_tg_base[Amine_name[1]][Epoxy_name[2]] = 185
 
 
 df_tg_base.to_csv('Tg_base.csv')
