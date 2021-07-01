273cc644780acec09658a895809aa19df5e6bd95
Ivan <ivan78.ru@yandex.ru>
2021-07-01 03:28:08 +0300

    Добавил тест на базовое стекло.

--- test_project.py
+++ test_project.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from Window_maker import MainWindow
+from Window_maker import MyMainWindow
 
 
 import pytest
@@ -18,7 +18,7 @@ import pytest
 def test_eew_a():
     app = QApplication(sys.argv)
     DB_NAME='material_for_test.db'
-    self = MainWindow(DB_NAME)
+    self = MyMainWindow(DB_NAME)
     self.add_line("A")
     self.add_line("A")
     self.material_a_types[0].setCurrentIndex(2)
@@ -34,7 +34,7 @@ def test_eew_a():
 def test_eew_b():
     app = QApplication(sys.argv)
     DB_NAME='material_for_test.db'
-    self = MainWindow(DB_NAME)
+    self = MyMainWindow(DB_NAME)
     self.add_line("B")
     self.add_line("B")
     self.material_b_types[0].setCurrentIndex(2)
@@ -49,7 +49,7 @@ def test_eew_b():
 def test_ahew_a():
     app = QApplication(sys.argv)
     DB_NAME='material_for_test.db'
-    self = MainWindow(DB_NAME)
+    self = MyMainWindow(DB_NAME)
     self.add_line("A")
     self.add_line("A")
     self.material_a_types[0].setCurrentIndex(1)
@@ -64,7 +64,7 @@ def test_ahew_a():
 def test_ahew_b():
     app = QApplication(sys.argv)
     DB_NAME='material_for_test.db'
-    self = MainWindow(DB_NAME)
+    self = MyMainWindow(DB_NAME)
     self.add_line("B")
     self.add_line("B")
     self.material_b_types[0].setCurrentIndex(1)
@@ -74,3 +74,27 @@ def test_ahew_b():
     self.material_percent_lines_b[1].setText("50.00")
     self.count_ew("B")
     assert self.ew_b == -34.285714285714285
+
+
+def test_primary_tg():
+    app = QApplication(sys.argv)
+    DB_NAME='material_for_test.db'
+    self = MyMainWindow(DB_NAME)
+    self.add_line("A")
+    self.add_line("A")
+    self.add_line("B")
+    self.add_line("B")
+    self.material_a_types[0].setCurrentIndex(2)
+    self.material_a_types[1].setCurrentIndex(2)
+    self.material_b_types[0].setCurrentIndex(1)
+    self.material_b_types[1].setCurrentIndex(1)
+    self.material_comboboxes_a[0].setCurrentIndex(0)
+    self.material_comboboxes_a[1].setCurrentIndex(1)
+    self.material_comboboxes_b[0].setCurrentIndex(0)
+    self.material_comboboxes_b[1].setCurrentIndex(1)
+    self.material_percent_lines_b[0].setText("50.00")
+    self.material_percent_lines_b[1].setText("50.00")
+    self.material_percent_lines_a[0].setText("50.00")
+    self.material_percent_lines_a[1].setText("50.00")
+    self.count_all_parameters()
+    assert self.primary_tg == 117.1
